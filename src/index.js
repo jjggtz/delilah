5cef138de5aa90db9f1cabc3136f98af2e47de19
jjggtz <60077846+jjggtz@users.noreply.github.com>
2020-05-13 15:04:16 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MySqlStore = require('express-mysql-session');
+
+const { database } = require('./keys');
+console.log(database);
+
+//inicializaciones
+const app = express();
+require('./lib/passport');
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs', 
+//    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+//middlewares
+app.use(session({
+    secret: 'delilahnodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySqlStore(database)
+}));
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// variables globales
+app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
+    app.locals.message = req.flash('message');
+    app.locals.user = req.user;
+    next();
+})
+
+//routes
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
+
+//roles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting the server
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
